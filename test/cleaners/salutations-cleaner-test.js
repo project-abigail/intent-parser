32a1e43884d6ccf25964eec6e3117404e101715d
Guillaume Marty <edo999@gmail.com>
2016-10-06 13:58:38 +0100

    Add tests for the salutations cleaner

---
+++ test/cleaners/salutations-cleaner-test.js
@@ -0,0 +1,64 @@
+import assert from 'assert';
+import SalutationsCleaner from '../../src/cleaners/salutations-cleaner';
+
+describe('SalutationsCleaner', () => {
+  describe('Removes salutations at the beginning of phrases.', () => {
+    const fixtures = [
+      [
+        'Hello how are you?',
+        'how are you?',
+      ],
+      [
+        'Hey how are you?',
+        'how are you?',
+      ],
+      [
+        'Hi how are you?',
+        'how are you?',
+      ],
+      [
+        'Yo how are you?',
+        'how are you?',
+      ],
+    ];
+
+    fixtures.forEach(([input, output]) => {
+      it(output, () => {
+        const salutationsCleaner = new SalutationsCleaner();
+        return salutationsCleaner.clean({ cleaned: input }).then((result) => {
+          assert.equal(result.cleaned, output);
+        });
+      });
+    });
+  });
+
+  describe('Removes leading salutations and punctuations from phrases.', () => {
+    const fixtures = [
+      [
+        'Hey! How are you?',
+        'How are you?',
+      ],
+      [
+        'Hey, how are you?',
+        'how are you?',
+      ],
+      [
+        'Hey!! how are you?',
+        'how are you?',
+      ],
+      [
+        'Hey. How are you?',
+        'How are you?',
+      ],
+    ];
+
+    fixtures.forEach(([input, output]) => {
+      it(output, () => {
+        const salutationsCleaner = new SalutationsCleaner();
+        return salutationsCleaner.clean({ cleaned: input }).then((result) => {
+          assert.equal(result.cleaned, output);
+        });
+      });
+    });
+  });
+});
