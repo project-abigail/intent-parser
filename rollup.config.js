f919089f221d26df771849be2fa10898bd28650e
Guillaume Marty <edo999@gmail.com>
2016-10-06 12:05:02 +0100

    Minify build files

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,26 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/intent-parser.js',
   format: 'umd',
-  plugins: [babel({
-      babelrc: false,
-      presets: [
-        "es2015-rollup"
-      ],
-      plugins: [
-        [
-          "transform-es2015-classes",
-          {
-            "loose": true
-          }
+  plugins: [
+    babel({
+        babelrc: false,
+        presets: [
+          "es2015-rollup"
+        ],
+        plugins: [
+          [
+            "transform-es2015-classes",
+            {
+              "loose": true
+            }
+          ]
         ]
-      ]
-    }
-  )],
+      }
+    ),
+    uglify()],
   moduleName: 'IntentParser',
   external: [
     'chrono-node',
