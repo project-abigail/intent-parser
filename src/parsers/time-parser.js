e0139d9203b6ec1e0484016562c86db81156a8fe
Julien Wajsberg <felash@gmail.com>
2016-11-16 15:50:39 +0100

    Fix last comment

--- src/parsers/time-parser.js
+++ src/parsers/time-parser.js
@@ -32,7 +32,7 @@ export default class TimeParser {
       const afterText = text.substr(date.index + extractedText.length).trim();
 
       // temporary fix for https://github.com/wanasit/chrono/issues/152
-      if (/ (?:on|by)$/.test(beforeText)) {
+      if (/\b(?:on|by)$/.test(beforeText)) {
         beforeText = beforeText.slice(0, -3);
       }
 
